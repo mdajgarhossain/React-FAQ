75f4fd3fa0e759fe69952825731122accd6fac83
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-03-31 21:38:41 +0600

    State in react

--- src/components/Parent/Parent.js
+++ src/components/Parent/Parent.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './Parent.css';
 import Child from '../Child/Child';
+import { useState } from 'react';
 
 function Parent() {
+    const [count, setCount] = useState(0);
+
+    const handleIncrement = () => {
+        const newCount = count + 1;
+        setCount(newCount);
+        console.log('Increment');
+    };
+    const handleDecrement = () => {
+        const newCount = count - 1;
+        setCount(newCount);
+        console.log('Decrement');
+    };
+
     return (
         <div className="parent">
             <h1>I am Parent component!</h1>
+            <p>Count: {count}</p>
+            <button onClick={handleIncrement}>Increment</button>
+            <button onClick={handleDecrement}>Decrement</button>
             <Child />
         </div>
     )
