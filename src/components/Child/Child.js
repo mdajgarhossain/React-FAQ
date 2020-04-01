6d02ce6d241af34d682f55fc5a67ce2e5df7d117
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-04-01 16:43:25 +0600

    Different ways to pass data as props from parent to Child

--- src/components/Child/Child.js
+++ src/components/Child/Child.js
@@ -2,8 +2,14 @@ import React from 'react';
 import './Child.css';
 
 function Child(props) {
-    // console.log(props);
-    const {count, handleIncrement, handleDecrement} = props;
+    console.log(props);
+    const {count, handleIncrement, handleDecrement, courses, handleCourses, developer, handleDeveloper} = props;
+
+    const fullstack = {
+        name: 'Jewell',
+        age: 26,
+        stack: 'Fullstack'
+    };
     
     return (
         <div className="child">
@@ -11,6 +17,18 @@ function Child(props) {
             <p>Count: {count}</p>
             <button onClick={handleIncrement}>Increment</button>
             <button onClick={handleDecrement}>Decrement</button>
+            <br/>
+            <h5 onClick={handleCourses}>Click here to see all courses: </h5>
+            <ul>
+                {
+                    courses.map(course => <li>{course}</li>)
+                }
+            </ul>
+            <button onClick={() => handleDeveloper(fullstack)}>Developer Details</button>
+            <div className="showDeveloper">
+                <p>Developer Name: {developer.name}</p>
+                <p>Developer Age: {developer.age}</p>
+            </div>
         </div>
     )
 }
--- src/components/Parent/Parent.js
+++ src/components/Parent/Parent.js
@@ -5,6 +5,10 @@ import { useState } from 'react';
 
 function Parent() {
     const [count, setCount] = useState(0);
+    const [courses, setCourses] = useState([]);
+    const [developer, setDeveloper] = useState({});
+
+    const ourCourses = ['Javascript', 'React', 'Redux'];
 
     const handleIncrement = () => {
         const newCount = count + 1;
@@ -17,6 +21,18 @@ function Parent() {
         console.log('Decrement');
     };
 
+    const handleCourses = () => {    
+        const newCourses = [...courses, ...ourCourses];
+        setCourses(newCourses);
+    };
+
+    const handleDeveloper = developer => {
+        const newDeveloper = {...developer};
+        setDeveloper(newDeveloper);
+        console.log(developer);
+        
+    };
+
     return (
         <div className="parent">
             <h1>I am Parent component!</h1>
@@ -27,6 +43,10 @@ function Parent() {
                 count={count}
                 handleIncrement={handleIncrement}
                 handleDecrement={handleDecrement}
+                courses={courses}
+                handleCourses={handleCourses}
+                developer={developer}
+                handleDeveloper={handleDeveloper}
             />
         </div>
     )
